d935e546e8fd183f17966c4e4c6c0db67cad4dc7
Mark Theunissen <mark.theunissen@gmail.com>
2014-04-14 12:09:06 +0200

    Adding go

---
+++ golang.go
@@ -0,0 +1,17 @@
+// This package
+package config
+
+// Package import
+import (
+  "encoding/json"
+  "errors"
+  "fmt"
+  "io/ioutil"
+)
+
+// Printing out for debugging
+// Print a string
+fmt.Println(name)
+
+// Print a struct
+fmt.Printf("%+v", user)
